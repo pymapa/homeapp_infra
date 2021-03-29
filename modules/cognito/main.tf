2302f76b39067a77032c7f68faf8aede7a9d7c1d
Pyry Pajunpää <pyry@identio.fi>
2021-03-29 14:41:22 +0300

    cognito pool attributes

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -52,7 +52,7 @@ resource "aws_cognito_user_pool_client" "client" {
 
   name                   = "homeapp-client-${var.env}"
   refresh_token_validity = 30
-  read_attributes        = ["nickname"]
+  read_attributes        = ["nickname", "email"]
   write_attributes       = ["nickname", "name", "family_name"]
 
   supported_identity_providers = ["COGNITO"]
